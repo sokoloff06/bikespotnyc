2231a3c874588689de23e9bf27a9373f1886d729
Vitaly Sokolov <sokoloff06@gmail.com>
2025-09-20 18:34:52 -0400

    Package name cleanup

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -15,7 +15,7 @@ if (keystorePropertiesFile.exists()) {
 }
 
 android {
-    namespace = "com.example.bikespotnyc"
+    namespace = "com.visokolov.bikespotnyc"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -30,7 +30,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.bikespotnyc"
+        applicationId = "com.visokolov.bikespotnyc"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
